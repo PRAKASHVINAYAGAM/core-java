19ad6ab07e1f85389a3a60a7ff4fe86936515aec
prakash <165361351+PRAKASHVINAYAGAM@users.noreply.github.com>
2024-12-23 11:40:38 +0530

    Update instance.java
    Hi, boobalan
     I added subtraction program in your program.

--- instance.java
+++ instance.java
@@ -5,12 +5,20 @@ public class instance {
     {
         System.out.println(c);
     }
+    static void subtraction(){
+        int e=20;
+        int f =30;
+        int g;
+        g=f-e;
+        System.out.println("subtraction from f to e - "+g);
+    }
 
 public static void main(String[] args) {
 
     instance d = new instance();
     System.out.println(d.c);
     d.print();
+    instance.subtraction();
 }
 
 }
